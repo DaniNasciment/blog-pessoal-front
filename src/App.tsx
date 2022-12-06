c2404e2458312344b581f95b7f150e8f1f268c39
DaniNasciment <dannni01@hotmail.com>
2022-12-06 16:10:23 -0300

    feat:ultima parte do crud

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer } from './components/estaticos/Footer/Footer';
 import { Navbar } from './components/estaticos/navbar/Navbar';
+import { CadastroUsuario } from './components/paginas/cadastroUsuario/CadastroUsuario';
 import { Home } from './components/paginas/home/Home';
-import Login from './components/paginas/login/Login';
+import {Login} from './components/paginas/login/Login';
 
 
 function App() {
@@ -13,16 +14,20 @@ function App() {
       <Navbar />
       <Routes>
 
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login  />} />
 
         <Route path="/home" element={<Home />} />
 
         <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastroUsuario" element={<CadastroUsuario/>} />
+
       </Routes>
       <Footer />
     </Router>
 
   );
 }
+
 
 export default App;
